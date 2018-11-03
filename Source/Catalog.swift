b6091d62938fe38cad1b331ca2a3ed56273371e4
vauxhall <zero.griffin@gmail.com>
2018-11-03 11:43:56 +0100

    Filter and merge tractors
    We should only keep reference to the last position
    of every driver to avoid having to iterate through
    all of them every time we need to update tractors.
    
    A map of every driver should be store also, so we
    don't lose reference to them even if in the next
    request a driver doesn't come up in the json we can
    still have a reference from it.

--- Source/Catalog.swift
+++ Source/Catalog.swift
@@ -1,19 +1,54 @@
 import Foundation
 
 public class Catalog {
-    var drivers = [Driver]()
-    var positions = [Position]()
+    public var tractors = [Tractor]()
+    private var driversMap = [String:Driver]() { didSet { updateTractors() } }
+    private var positionsMap = [String:Position]() { didSet { updateTractors() } }
+    private let formatter = DateFormatter()
     
-    public init() { }
+    public init() {
+        formatter.dateFormat = "yyyy-MM-dd'T'hh:mm:ss.SSS'Z'"
+        formatter.timeZone = TimeZone(identifier:"UTC")
+    }
     
-    public var tractors:[Tractor] {
-        return drivers.compactMap{ driver in
-            guard let position = positions.first(where:{ $0.driver_id == driver.id }) else { return nil }
+    func update(drivers:[Driver]) {
+        var driversMap = self.driversMap
+        drivers.forEach { driversMap[$0.id] = $0 }
+        self.driversMap = driversMap
+    }
+    
+    func update(positions:[Position]) {
+        var positionsMap = self.positionsMap
+        positions.forEach { position in
+            if let oldPosition = positionsMap[position.driver_id] {
+                positionsMap[position.driver_id] = mostRecent(positionA:position, positionB:oldPosition)
+            } else {
+                positionsMap[position.driver_id] = position
+            }
+        }
+        self.positionsMap = positionsMap
+    }
+    
+    private func updateTractors() {
+        tractors = driversMap.compactMap{ id, driver in
+            guard
+                driver.active,
+                let position = positionsMap[id]
+            else { return nil }
             var tractor = Tractor()
             tractor.driver = driver.name
             tractor.latitude = position.latitude
             tractor.longitude = position.longitude
             return tractor
         }
+    }
+    
+    private func mostRecent(positionA:Position, positionB:Position) -> Position {
+        guard
+            let dateA = formatter.date(from:positionA.timestamp),
+            let dateB = formatter.date(from:positionB.timestamp),
+            dateA > dateB
+        else { return positionB }
+        return positionA
     }
 }
--- Tests/TestMerging.swift
+++ Tests/TestMerging.swift
@@ -9,10 +9,49 @@ class TestMerging:XCTestCase {
     }
     
     func testGetTractorInfo() {
-        catalog.drivers = [Driver(id:"a", name:"John", active:true)]
-        catalog.positions = [Position(driver_id:"a", latitude:1, longitude:2, timestamp:"0")]
+        catalog.update(drivers:[Driver(id:"a", name:"John", active:true)])
+        catalog.update(positions:[Position(driver_id:"a", latitude:1, longitude:2, timestamp:String())])
         XCTAssertEqual("John", catalog.tractors.first!.driver)
         XCTAssertEqual(1, catalog.tractors.first!.latitude)
         XCTAssertEqual(2, catalog.tractors.first!.longitude)
+    }
+    
+    func testIgnoreUnusedPositions() {
+        catalog.update(drivers:[Driver(id:"a", name:"John", active:true)])
+        catalog.update(positions:[Position(driver_id:"b", latitude:4, longitude:5, timestamp:String()),
+                                  Position(driver_id:"a", latitude:1, longitude:2, timestamp:String()),
+                                  Position(driver_id:"c", latitude:3, longitude:6, timestamp:String())])
+        XCTAssertEqual(1, catalog.tractors.count)
+        XCTAssertEqual("John", catalog.tractors.first!.driver)
+        XCTAssertEqual(1, catalog.tractors.first!.latitude)
+        XCTAssertEqual(2, catalog.tractors.first!.longitude)
+    }
+    
+    func testIgnoreDriverNoPosition() {
+        catalog.update(drivers:[Driver(id:"b", name:"Dan", active:true),
+                                Driver(id:"a", name:"John", active:true),
+                                Driver(id:"c", name:"Anne", active:true)])
+        catalog.update(positions:[Position(driver_id:"a", latitude:1, longitude:2, timestamp:String())])
+        XCTAssertEqual(1, catalog.tractors.count)
+        XCTAssertEqual("John", catalog.tractors.first!.driver)
+    }
+    
+    func testOnlyActiveDrivers() {
+        catalog.update(drivers:[Driver(id:"b", name:"Dan", active:false),
+                                Driver(id:"a", name:"Anne", active:true)])
+        catalog.update(positions:[Position(driver_id:"a", latitude:0, longitude:0, timestamp:String()),
+                                  Position(driver_id:"b", latitude:0, longitude:0, timestamp:String())])
+        XCTAssertEqual(1, catalog.tractors.count)
+        XCTAssertEqual("Anne", catalog.tractors.first!.driver)
+    }
+    
+    func testOnlyLastPosition() {
+        catalog.update(drivers:[Driver(id:"a", name:"Marie", active:true)])
+        catalog.update(positions:
+            [Position(driver_id:"a", latitude:3, longitude:4, timestamp:"2018-06-20T04:58:12.999Z"),
+             Position(driver_id:"a", latitude:6, longitude:5, timestamp:"2018-06-20T04:58:13.999Z"),
+             Position(driver_id:"a", latitude:7, longitude:8, timestamp:"2018-06-20T04:58:11.999Z")])
+        XCTAssertEqual(6, catalog.tractors.first!.latitude)
+        XCTAssertEqual(5, catalog.tractors.first!.longitude)
     }
 }
